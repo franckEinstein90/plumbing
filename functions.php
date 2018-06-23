4e3d71f4782d7b13f9ea1fc5d3c5015ca750cc48
root <root@partnersio.c.extended-ascent-140621.internal>
2018-06-23 02:49:22 +0000

    added match function

--- functions.php
+++ functions.php
@@ -1,13 +1,40 @@
 <?php
+
+function askDB($type, $objID, $db){
+	$sql = "SELECT password FROM $type WHERE ".strtolower($type)."= '".$objID."'"; 
+	$n = $db->get_var($sql);
+	return $n;
+}
+
+function evalExpression($expr, $db){
+	preg_match("/(.+\))\.(.+)/", $expr, $re);
+	$row = $re[1];
+	$field = $re[2];
+
+	preg_match("/(.+)\((.+)\)/", $row, $rowRe);
+	$type = $rowRe[1];
+	$objID = $rowRe[2];
+	return askDB($type, $objID,  $db);
+
+	}
+	function match($expression, $value, $db){
+		$expressionValue = evalExpression($expression, $db);
+		//echo "matching ".$expressionValue ." with ".$value;
+		return ($expressionValue == $value);
+	}
+
 	function dbif($statement, $db){
 		$tokens = preg_split("/[\s]+/",$statement);
 		switch ($tokens[0]){
-		case "EXISTS":
+		case "exists":
 			$Type = strtoupper($tokens[1]);
 			$VAL = $tokens[2];
 			$sql = "SELECT count(*) FROM ". $Type ." WHERE " .strtolower($Type)." = '".$VAL."'";
 			$n = $db->get_var($sql);
 			return $n; 
+			break;
+		case "match":
+			return (match ($tokens[1],  $tokens[2], $db));
 			break;
 		default:
 			return 0;
